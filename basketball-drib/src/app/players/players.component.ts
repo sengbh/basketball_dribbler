3d7f1e58191b8b2703e4d4719f3c76e1eceb5832
sengbh <sengbh@ieee.org>
2019-11-24 15:43:48 -0600

    added players statically

--- basketball-drib/src/app/players/players.component.ts
+++ basketball-drib/src/app/players/players.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerClass} from '../player';
+import {PLAYERS} from '../player-list';
+
 
 @Component({
   selector: 'app-players',
@@ -6,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
+
+  //hardcode data
+  // private player: PlayerClass = {
+  //   keyCode: 'A',
+  //   name: 'player 1',
+  //   color: '#BD10E0'
+  // };
+  private players: PlayerClass[] = PLAYERS;
 
   constructor() { }
 
