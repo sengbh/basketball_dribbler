e5972b63a9a356176dd2e641732aeb6ad34ed4bf
sengbh <sengbh@ieee.org>
2019-11-24 16:16:06 -0600

    added service component to get data across

---
+++ basketball-drib/src/app/player.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {PlayerClass} from './player';
+import {PLAYERS} from './player-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+
+  constructor() { }
+}
